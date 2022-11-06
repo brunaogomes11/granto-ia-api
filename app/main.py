2a92e937c066e66b55cf7900bcbd01e61a9ccabc
Bruno Gomes <74106187+brunaogomes11@users.noreply.github.com>
2022-11-06 08:24:40 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def servidorLigado():
     return "Servidor Rodando"
 
-@app.route("/testar/<genero>/<idade>/<debito>/<casado>/<contaBanco>/<tipo_industria>/<etnia>/<anos_empregado>/<inadimplenia>/<empregado>/<pont_credito>/<cnh>/<cidadania>/<cep>/<renda>", methods=["GET"])
+@app.route("/testar/<genero>/<idade>/<debito>/<casado>/<contaBanco>/<tipo_industria>/<etnia>/<anos_empregado>/<inadimplencia>/<empregado>/<pont_credito>/<cnh>/<cidadania>/<cep>/<renda>", methods=["GET"])
 def criarModelos(genero, idade, debito, casado, contaBanco, tipo_industria, etnia, anos_empregado, inadimplencia, empregado, pont_credito, cnh, cidadania, cep, renda):
     if request.data:
         dados = [float(genero), float(idade), float(debito), float(casado), float(contaBanco), float(tipo_industria), float(etnia), float(anos_empregado), float(inadimplencia), float(empregado), float(pont_credito), float(cnh), float(cidadania), float(cep), float(renda)]
