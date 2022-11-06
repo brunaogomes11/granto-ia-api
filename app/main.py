20cf358680b497411c3ab97ce377a086bf5085c6
Bruno Gomes <74106187+brunaogomes11@users.noreply.github.com>
2022-11-06 10:59:40 -0300

    Backing to post method

--- app/main.py
+++ app/main.py
@@ -8,10 +8,26 @@ app = Flask(__name__)
 def servidorLigado():
     return "Servidor Rodando"
 
-@app.route("/testar/<genero>/<idade>/<debito>/<casado>/<contaBanco>/<tipo_industria>/<etnia>/<anos_empregado>/<inadimplencia>/<empregado>/<pont_credito>/<cnh>/<cidadania>/<cep>/<renda>", methods=["GET"])
-def criarModelos(genero, idade, debito, casado, contaBanco, tipo_industria, etnia, anos_empregado, inadimplencia, empregado, pont_credito, cnh, cidadania, cep, renda):
+@app.route("/testar", methods=["POST"])
+def criarModelos():
+    request.data = request.get_json()
     if request.data:
-        dados = [float(genero), float(idade), float(debito), float(casado), float(contaBanco), float(tipo_industria), float(etnia), float(anos_empregado), float(inadimplencia), float(empregado), float(pont_credito), float(cnh), float(cidadania), float(cep), float(renda)]
+        genero = request.data['Genero']
+        idade = request.data['Idade'] 
+        debito = request.data['Debito'] 
+        casado = request.data['Casado'] 
+        contaBanco = request.data['Possui_Conta_Banco']
+        tipo_industria = request.data['Tipo_Industria'] 
+        etnia = request.data['Etnia']
+        anos_empregado = request.data['Anos_Empregado'] 
+        inadimplencia = request.data['Inadimplente'] 
+        empregado = request.data['Empregado']
+        pont_credito = request.data['Pontuacao_Credito']
+        cnh = request.data['Habilitado']
+        cidadania = request.data['Cidadania']
+        cep = request.data['Codigo_Postal']
+        renda = request.data['Renda']
+        dados = [genero, idade, debito, casado, contaBanco, tipo_industria, etnia, anos_empregado, inadimplencia, empregado, pont_credito, cnh, cidadania, cep, renda]
         aprovado = predict(dados)
         return jsonify({'Aprovado':f'{aprovado}'}) 
 
