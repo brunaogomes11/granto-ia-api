2d15502142860a0653085f29727c4835cabd7493
Bruno Gomes <74106187+brunaogomes11@users.noreply.github.com>
2022-11-15 00:39:08 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from app.iaModel import predict
 import os
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=["GET"])
 def servidorLigado():
